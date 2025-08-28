694f472afcc98f8ae678674c15b7bcc168fd2e15
casperxs <casperxs@users.noreply.github.com>
2025-08-28 16:58:17 -0600

    colores naranjas

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
 
             <button
                 type="submit"
-                class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                class="w-full bg-orange-600 text-white py-2 px-4 rounded-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500"
             >
                 Iniciar Sesión
             </button>
