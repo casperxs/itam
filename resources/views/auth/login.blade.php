e0858675cd85789ffc01641ee523c943be132166
casperxs <casperxs@users.noreply.github.com>
2025-08-28 17:03:59 -0600

    regresamos a azules

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
                     id="email"
                     name="email"
                     value="{{ old('email') }}"
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     required
                 >
                 @error('email')
@@ -40,7 +40,7 @@
                     type="password"
                     id="password"
                     name="password"
-                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-orange-500"
+                    class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                     required
                 >
                 @error('password')
@@ -50,7 +50,7 @@
 
             <button
                 type="submit"
-                class="w-full bg-orange-600 text-white py-2 px-4 rounded-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-orange-500"
+                class="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
                 Iniciar Sesión
             </button>
